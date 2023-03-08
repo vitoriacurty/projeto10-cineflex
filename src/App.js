ab96ca7de4fd461280cc25e8e21bee2cf49b2b27
vitoriacurty <vitoria.curty@hotmail.com>
2023-03-08 16:54:45 -0300

    add import BrowserRouter, Routes, Route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage/HomePage"
 import SeatsPage from "./pages/SeatsPage/SeatsPage"
 import SessionsPage from "./pages/SessionsPage/SessionsPage"
