301dbd8c4aefc1b880512c847927e817dbbc307f
vitoriacurty <vitoria.curty@hotmail.com>
2023-03-11 00:22:01 -0300

    fix bug in route

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ export default function App() {
       <NavContainer>CINEFLEX</NavContainer>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/sessoes/:idFilme" element={<SeatsPage />} />
-        <Route path="/assentos/:idSessao" element={< SessionsPage />} />
+        <Route path="/sessoes/:idFilme" element={< SessionsPage />} />
+        <Route path="/assentos/:idSessao" element={<SeatsPage />} />
         <Route path="/sucesso" element={<SuccessPage />} />
       </Routes>
     </BrowserRouter>
