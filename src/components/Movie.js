6f8b015cffdbe1d7473c53740a20d450e599b012
vitoriacurty <vitoria.curty@hotmail.com>
2023-03-13 15:54:19 -0300

    create component Movie and Navbar

---
+++ src/components/Movie.js
@@ -0,0 +1,7 @@
+export const Movie = ({ src, title }) => {
+  return (
+    <>
+      <img src={src} alt={title}></img>
+    </>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+export const Navbar = ({title}) => {
+  return (
+    <NavContainer>{title}</NavContainer>
+  )
+}
+
+const NavContainer = styled.div`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #C3CFD9;
+  color: #E8833A;
+  font-family: 'Roboto', sans-serif;
+  font-size: 34px;
+  position: fixed;
+  top: 0;
+  a {
+      text-decoration: none;
+      color: #E8833A;
+  }
+`
