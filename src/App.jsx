4b17772386e55fc5b1d4b19af48f72450e314caa
vitoriacurty <vitoria.curty@hotmail.com>
2023-05-23 15:22:04 -0300

    adicionando layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components"
+import HomePage from "./pages/HomePage/HomePage"
+import SeatsPage from "./pages/SeatsPage/SeatsPage"
+import SessionsPage from "./pages/SessionsPage/SessionsPage"
+import SuccessPage from "./pages/SuccessPage/SuccessPage"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    return (
+        <>
+           <NavContainer>CINEFLEX</NavContainer>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <HomePage />
+            {/* <SeatsPage /> */}  
+            {/* <SessionsPage /> */}
+            {/* <SuccessPage /> */}
+        </>
+    )
 }
 
-export default App
+const NavContainer = styled.div`
+    width: 100%;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #C3CFD9;
+    color: #E8833A;
+    font-family: 'Roboto', sans-serif;
+    font-size: 34px;
+    position: fixed;
+    top: 0;
+    a {
+        text-decoration: none;
+        color: #E8833A;
+    }
+`
