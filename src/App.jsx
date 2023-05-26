3580870582759c7896d82706e35649a2d1b9a4b3
vitoriacurty <vitoria.curty@hotmail.com>
2023-05-26 19:37:36 -0300

    add componentes Footer e NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import styled from "styled-components"
 import axios from "axios"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage"
 import SeatsPage from "./pages/SeatsPage/SeatsPage"
 import SessionsPage from "./pages/SessionsPage/SessionsPage"
 import SuccessPage from "./pages/SuccessPage/SuccessPage"
+import NavBar from "./components/NavBar";
 
 export default function App() {
   axios.defaults.headers.common['Authorization'] = 'QoMHCmAWaMAAwNkO6sokJWv1'
@@ -12,7 +12,7 @@ export default function App() {
     <>
       <BrowserRouter>
       
-        <NavContainer>CINEFLEX</NavContainer>
+        <NavBar />
 
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -23,25 +23,8 @@ export default function App() {
 
       </BrowserRouter>
 
-
     </>
   )
 }
 
-const NavContainer = styled.div`
-    width: 100%;
-    height: 70px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #C3CFD9;
-    color: #E8833A;
-    font-family: 'Roboto', sans-serif;
-    font-size: 34px;
-    position: fixed;
-    top: 0;
-    a {
-        text-decoration: none;
-        color: #E8833A;
-    }
-`
+
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import { styled } from "styled-components"
+
+
+export default function NavBar() {
+    return (
+        <>
+        <NavContainer>CINEFLEX</NavContainer>
+        </>
+    )
+}
+
+const NavContainer = styled.div`
+    width: 100%;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #C3CFD9;
+    color: #E8833A;
+    font-family: 'Roboto', sans-serif;
+    font-size: 34px;
+    position: fixed;
+    top: 0;
+    a {
+        text-decoration: none;
+        color: #E8833A;
+    }
+`
